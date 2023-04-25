43be6c240baecb97b9ff46dab1e44b630d23f135
YagoQPio <126572216+YagoQPio@users.noreply.github.com>
2023-04-25 09:31:14 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -13,6 +13,17 @@ package q2
 //Ajude os amigos a encontrar o número de problemas para os quais eles escreverão uma solução.
 
 func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+count := 0
+	for i := 0; i < len(answers); i++ {
+		soma := 0
+		for j := 0; j < 3; j++ {
+			if answers[i][j] {
+				soma++
+			}
+		}
+		if soma >= 2 {
+			count++
+		}
+	}
+	return count
 }
