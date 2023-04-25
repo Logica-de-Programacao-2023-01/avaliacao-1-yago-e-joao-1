7d34b9ee5d365476b882349bca7061d079cf0fbd
YagoQPio <126572216+YagoQPio@users.noreply.github.com>
2023-04-25 10:07:56 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,5 +1,5 @@
 package q5
-import "fmt"
+import "strings"
 
 //Pedro começou a frequentar aulas de programação. Na primeira aula, sua tarefa foi escrever um programa simples. O
 //programa deveria fazer o seguinte: na sequência de caracteres fornecida, composta por letras latinas maiúsculas e
